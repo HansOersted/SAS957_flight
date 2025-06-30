9f1ae8b2a7cd38c8c81db98e4010c4c077c60ee7
HansOersted <lucidshenzhe@gmail.com>
2025-06-30 16:01:42 +0200

    correct the format of the data

--- for_Lyapunov_training.py
+++ for_Lyapunov_training.py
@@ -1,21 +1,21 @@
 import pandas as pd
 
-user_input_end_time = 15390  # select the end time for the subset, 
+user_input_end_time = 15450  # select the end time in seconds, e.g., 15450 seconds
 
-input_csv = "tracking_error_dynamics_6150s_to_19000s.csv"
-df = pd.read_csv(input_csv)
+df = pd.read_csv("tracking_error_dynamics_6150s_to_19000s.csv")
 
 closest_end_time = df["Time (s)"].iloc[(df["Time (s)"] - user_input_end_time).abs().argmin()]
-print(f"The closest end time is: {closest_end_time} s")
+print(f"Selected end time: {closest_end_time} s")
 
 subset_df = df[(df["Time (s)"] >= 6150) & (df["Time (s)"] <= closest_end_time)].copy()
 
-converted_df = subset_df.rename(columns={
-    "tracking_error": "e",
-    "tracking_error_derivative": "de",
-    "tracking_error_second_derivative": "dde"
-})[["Time (s)", "e", "de", "dde"]]
+packed_df = pd.DataFrame({
+    "e[0]": subset_df["tracking_error"],
+    "e[1]": subset_df["tracking_error_derivative"],
+    "de[0]": subset_df["tracking_error_derivative"],
+    "de[1]": subset_df["tracking_error_second_derivative"]
+})
 
-output_csv = f"lyapunov_training_data_6150s_to_{int(closest_end_time)}s.csv"
-converted_df.to_csv(output_csv, index=False)
+output_csv = f"packed_lyapunov_input_6150s_to_{int(closest_end_time)}s.csv"
+packed_df.to_csv(output_csv, index=False)
 print(f"Output file saved as: {output_csv}")
