11c5b9e372f0eb7df39b3a4aea55c21a71d32f6d
HansOersted <lucidshenzhe@gmail.com>
2025-09-12 12:52:24 +0800

    starting at 6210s

--- for_Lyapunov_training_multiple.py
+++ for_Lyapunov_training_multiple.py
@@ -3,15 +3,15 @@ import numpy as np
 
 df = pd.read_csv("tracking_error_dynamics_6150s_to_19000s.csv")
 
-for user_input_end_time in range(15450, 15480 + 1, 30):
+for user_input_end_time in range(15480, 15810 + 1, 30):
     closest_end_time = df["Time (s)"].iloc[(df["Time (s)"] - user_input_end_time).abs().argmin()]
     print(f"Selected end time: {closest_end_time} s")
 
-    subset_df = df[(df["Time (s)"] >= 6150) & (df["Time (s)"] <= closest_end_time)].copy()
+    subset_df = df[(df["Time (s)"] >= 6210) & (df["Time (s)"] <= closest_end_time)].copy()
 
     e = subset_df[["tracking_error", "tracking_error_derivative"]].to_numpy()
     de = subset_df[["tracking_error_derivative", "tracking_error_second_derivative"]].to_numpy()
 
-    output_filename = f"training_data_6150s_to_{int(closest_end_time)}s.npz"
+    output_filename = f"training_data_6210s_to_{int(closest_end_time)}s.npz"
     np.savez(output_filename, e=e, de=de)
     print(f"Training data saved as: {output_filename}")
